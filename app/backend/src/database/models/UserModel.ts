09ab8f676e8d713fcc866dc7096731832a0a648e
LucasLopes <lopes.sp.lucas@gmail.com>
2022-11-22 17:46:46 -0300

    create dockerFiles and users migration and model

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
